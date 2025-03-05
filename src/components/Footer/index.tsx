ec909467a9fe33d58954a7716c23c4f82a4157d1
Grishaev Vladimir <vladimir.grishaew@yande>
2025-03-05 18:50:51 +0300

    05/03

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 export const Footer = () => {
   return (
     <footer className={styles.container}>
-      © Created by Margarita Shpileva , 2024
+      © Created by Grishaev Vladimir , 2024
     </footer>
   );
 };
